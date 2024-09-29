c51ad2a59187c9801b95fe43eba1a5b852fdabfb
mkrtchianart <mkrtchianart@gmail.com>
2024-09-30 00:19:15 +0400

    Add files via upload

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,7 +5,7 @@ export default function ProfilePage() {
   return (
     <div className="min-h-screen bg-[#F8F8F7] flex flex-col items-center justify-center p-4">
       <h1 className="text-3xl font-bold mb-4">User&apos;s Profile</h1>
-      <p className="text-xl mb-8">This is where you'd see your profile information.</p>
+      <p className="text-xl mb-8">This is where you&apos;d see your profile information.</p>
       <Link href="/" passHref>
         <Button className="bg-[#da7756] hover:bg-[#bd5d3a] text-white">
           Back to Game
